d01cadfb3411daeb41a5fd680e555a0b5cf23174
elisio <elisiorcsilva@hotmail.com>
2022-07-18 02:12:26 -0300

    First commit

---
+++ namingservice/src/test/java/br/com/veiculo/namingservice/NamingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.veiculo.namingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
